e99fc198544d4dd251201fc94302929040266df6
shxmrocks <utsumiiaoba@gmail.com>
2026-07-01 06:08:05 -0400

    Create upgrades.lua

---
+++ data/upgrades.lua
@@ -0,0 +1,154 @@
+return {
+    Casual = {
+        ["Adrenaline"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["BetterJumpPad"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["MoreAltars"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["LastManStanding"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["BusinessLicense"] = {
+            MaxStack = 2,
+            Client = false
+        },
+        ["EnemyOnTop"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["GraceWings"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["HighlightGifts"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["IceSkates"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["MatrixTetrahedron"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["PanicNecklace"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["Paycheck"] = {
+            MaxStack = 5,
+            Client = false
+        },
+        ["Shield"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["Helmet"] = {
+            MaxStack = 1,
+            Client = true
+            },
+        ["SportShoes"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["GiftMagnet"] = {
+            MaxStack = 3,
+            Client = true
+        },
+        ["PocketBell"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["GrapplePoints"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["SubspacialBarrier"] = {
+            MaxStack = 2,
+            Client = false
+        },
+        ["Medal"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["TheOrb"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["NinjaBelt"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["MiniatureHourglass"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["FannyPack"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["DoubleJump"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["RadarPlayer"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["RadarAltars"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["SwiftnessRing"] = {
+            MaxStack = 3,
+            Client = true
+        },
+        ["TriaOrbs"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["SharkTail"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["LargerGrapplePoints"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["GiftIdol"] = {
+            MaxStack = 5,
+            Client = false
+        },
+        ["AdvancedGravityCoil"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["AltarOfVoid"] = {
+            MaxStack = 1,
+            Client = false
+        },
+        ["RadarInstruments"] = {
+            MaxStack = 1,
+            Client = true
+        }
+    }
+    Other = {
+        ["HighlightTripmines"] = {
+            MaxStack = 1,
+            Client = true
+        },
+        ["DefuseKit"] = {
+            MaxStack = 3,
+            Client = false
+        },
+    }
+}
